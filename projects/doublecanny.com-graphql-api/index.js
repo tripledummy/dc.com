99cc4bb9344b751fc232c1940c0028c3922614e3
Davide Pirola <ziocleto@gmail.com>
2020-04-02 04:29:45 +0100

    adding websocket layer

--- projects/doublecanny.com-graphql-api/index.js
+++ projects/doublecanny.com-graphql-api/index.js
@@ -3,6 +3,7 @@ import dbi from "eh_db";
 import * as authController from "eh_auth_and_auth/controllers/authController";
 import {initApollo} from "./apolloServer";
 import {app, initServer, runServer} from "./httpserver";
+import {runWebsocketServer} from "./websocketServer";
 
 const usersRoute = require("eh_auth_and_auth/routes/usersRoute");
 const tokenRoute = require("eh_auth_and_auth/routes/tokenRoute");
@@ -23,3 +24,4 @@ const use = () => {
 init();
 use();
 runServer();
+runWebsocketServer();
